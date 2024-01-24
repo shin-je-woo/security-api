da699e5e0445f7089b125676299e0adf5dde8aa6
shin-je-woo <shinjw0926@naver.com>
2024-01-24 22:24:18 +0900

    fix: 초기데이터 INSERT시 동시성 이슈
    roles테이블의 데이터를 select할 때 비관적 락을 건다.
    트랜잭션이 작업중일 때 쓰기락을 걸어 다른 트랜잭션의 접근을 제한한다.
    
    fix #1

--- src/main/java/com/jewoos/securityapi/repository/role/RoleRepository.java
+++ src/main/java/com/jewoos/securityapi/repository/role/RoleRepository.java
@@ -2,11 +2,14 @@ package com.jewoos.securityapi.repository.role;
 
 import com.jewoos.securityapi.entity.Role;
 import com.jewoos.securityapi.enums.RoleType;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Role> findByRoleType(RoleType roleType);
 }
