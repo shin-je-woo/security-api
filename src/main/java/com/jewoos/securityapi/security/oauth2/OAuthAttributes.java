8d6591c0a9b510542ecc58de5bd142d78da49ffa
shin-je-woo <shinjw0926@naver.com>
2024-02-01 23:59:34 +0900

    feat: 소셜로그인 - 카카오

--- src/main/java/com/jewoos/securityapi/security/oauth2/OAuthAttributes.java
+++ src/main/java/com/jewoos/securityapi/security/oauth2/OAuthAttributes.java
@@ -25,6 +25,18 @@ public enum OAuthAttributes {
                 response.get("email").toString(),
                 response.get("name").toString()
         );
+    }),
+
+    KAKAO(attributes -> {
+        @SuppressWarnings("unchecked") // 카카오에서 보내온 api가 변경되지 않으면 타입캐스팅 안전
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        @SuppressWarnings("unchecked") // 카카오에서 보내온 api가 변경되지 않으면 타입캐스팅 안전
+        Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
+        return new OAuth2UserInfo(
+                profile.get("nickname").toString(),
+                profile.get("nickname").toString(),
+                profile.get("nickname").toString()
+        );
     });
 
     private final Function<Map<String, Object>, OAuth2UserInfo> getOAuth2UserInfo;
