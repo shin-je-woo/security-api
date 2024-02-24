b0f539bbbdc3fff564837256984c46ca1c40ceca
shin.jewoo <shinjw0926@naver.com>
2024-02-24 23:48:36 +0900

    feat: CD workflow 추가

---
+++ src/test/java/com/jewoos/securityapi/ExampleTest.java
@@ -0,0 +1,27 @@
+package com.jewoos.securityapi;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+
+//    @Test
+//    @DisplayName("실패하는 테스트")
+//    void test() {
+//        assertEquals(1, 2);
+//    }
+
+    @Test
+    @DisplayName("성공하는 테스트111")
+    void test2() {
+        assertEquals(1, 1);
+    }
+
+//    @Test
+//    @DisplayName("실패하는 테스트2")
+//    void test3() {
+//        assertEquals(1, 3);
+//    }
+}
